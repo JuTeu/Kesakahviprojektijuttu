d5128e9e75cb8ce6a66be6d723d1a4d780f4b9cf
Esa-Pekka Palonen <esa-pekka.palonen@wpk.tpu.fi>
2023-06-02 15:37:12 +0300

    chanting_ääniä, taikasauva luoti ja vihollisen tuhoaminen

--- Assets/EP_codestuff/Bullet.cs
+++ Assets/EP_codestuff/Bullet.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     [Range(1, 10)]
-    [SerializeField] private float speed = 10f;
+    [SerializeField] private float speed = 10;
 
     [Range(1, 10)]
     [SerializeField] private float lifeTime = 3f;
@@ -17,6 +17,7 @@ public class Bullet : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         // destroyes the gameobject after certain time has passed
         Destroy(gameObject, lifeTime);
+
     }
 
     // making the bullet go straigth all the time.
@@ -25,4 +26,14 @@ public class Bullet : MonoBehaviour
         rb.velocity = transform.up * speed;
     
     }
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+
+        if (collider.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(collider.gameObject);
+            Destroy(gameObject);
+        }
+    }
+
 }
--- Assets/EP_codestuff/Gun.cs
+++ Assets/EP_codestuff/Gun.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class Gun : MonoBehaviour
@@ -8,19 +10,28 @@ public class Gun : MonoBehaviour
     // change the bullet prefab to the one you want
     [SerializeField] private GameObject bulletPrefab;
     [SerializeField] private Transform firingPoint;
-    [Range(0.1f, 1f)] 
-    [SerializeField] private float firingRate = 0.5f;
+    [Range(0.1f, 1f)]
+    [SerializeField] private float fireRate = 0.5f;
+
+    private float fireTimer;
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && fireTimer <= 0f)
         {
             Shoot();
+            fireTimer = fireRate;
+        } else {
+            fireTimer -= Time.deltaTime;
         }
     }
+
     private void Shoot()
     {
-            Instantiate(bulletPrefab, firingPoint.position, firingPoint.rotation);
+        Instantiate(bulletPrefab, firingPoint.position, firingPoint.rotation);
         }
     }
+
+
+
 
 
