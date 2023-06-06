0a7f9253fb066d4cb279a6585cb1d0c260672597
SaimiVilkkilä <90615101+pupuDeveloper@users.noreply.github.com>
2023-06-06 10:51:08 +0300

    UI elementit pitäis skaalata nyt ruudun koon mukaan
    nappula yms liikkuu oikein. tekstit vielä on huonot eri ruudun koilla

--- Assets/saimiCode/coffeeMenuButton.cs
+++ Assets/saimiCode/coffeeMenuButton.cs
@@ -8,28 +8,31 @@ public class coffeeMenuButton : MonoBehaviour
     public GameObject coffeeMenu;
     public GameObject coffeeMenuSlider;
     public TMP_Text arrow;
+
     private void Start()
     {
         arrow.text = "<-";
-        this.gameObject.transform.position = new Vector2 (1869, 904);
+        this.gameObject.SetActive(true);
         coffeeMenu.SetActive(false);
         coffeeMenuSlider.SetActive(false);
     }
     public void openAndCloseCoffeeMenu()
     {
+        var menuButton = this.gameObject.GetComponent<RectTransform>();
+        var pos = menuButton.anchoredPosition;
         if (coffeeMenu.activeSelf && coffeeMenuSlider.activeSelf)
         {
+        menuButton.anchoredPosition = new Vector2(0, pos.y);
         arrow.text = "<-";
         coffeeMenu.SetActive(false);
         coffeeMenuSlider.SetActive(false);
-        this.gameObject.transform.position = new Vector2 (1869, 904);
         }
         else
         {
+        menuButton.anchoredPosition = new Vector2(-565, pos.y);
         arrow.text = "->";
         coffeeMenu.SetActive(true);
         coffeeMenuSlider.SetActive(true);
-        this.gameObject.transform.position = new Vector2 (1305, 904);
         }
     }
 }
