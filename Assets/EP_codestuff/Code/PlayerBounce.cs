4adadc50d238127ae0b9eafe75c9f955c58980fb
Esa-Pekka Palonen <esa-pekka.palonen@wpk.tpu.fi>
2023-06-09 12:59:47 +0300

    level desing

---
+++ Assets/EP_codestuff/Code/PlayerBounce.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBounce : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float launchForce; 
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            rb.velocity = Vector2.up * launchForce;      
+        }
+    }
+}
