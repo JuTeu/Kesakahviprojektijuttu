e40655cf24948710ad5c23b25a1f25e1d6e3460d
Esa-Pekka Palonen <esa-pekka.palonen@wpk.tpu.fi>
2023-06-13 10:04:07 +0300

    level design + enemytriangle

---
+++ Assets/EP_codestuff/Code/EnemyMovement.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public Transform[] patrolPoints;
+    public float moveSpeed;
+    public int patrolDestination;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        // Check the patrol destination of the enemy
+        if (patrolDestination == 0)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, patrolPoints[0].position, moveSpeed * Time.deltaTime);
+            if (Vector2.Distance(transform.position, patrolPoints[0].position)< .2f)
+            {
+                patrolDestination = 1;
+
+            }
+
+        }
+        if (patrolDestination == 1)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, patrolPoints[1].position, moveSpeed * Time.deltaTime);
+            if (Vector2.Distance(transform.position, patrolPoints[1].position) < .2f)
+            {
+                patrolDestination = 0;
+
+            }
+
+        }
+    }
+
+}
