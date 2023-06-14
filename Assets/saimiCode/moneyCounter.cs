e636b46a7cb8d8de7b0d4366ce0c25825169ac1d
SaimiVilkkilä <90615101+pupuDeveloper@users.noreply.github.com>
2023-06-14 12:06:57 +0300

    alotin kahvilan UI:n ja rahalaskurin
    ei toimi täysin vielä

--- Assets/saimiCode/moneyCounter.cs
+++ Assets/saimiCode/moneyCounter.cs
@@ -2,13 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using unity.UI;
+using Unity.UI;
 
 public class moneyCounter : MonoBehaviour
 {
     public TextMeshProUGUI moneyText;
-    private int moneyToBeAdded;
+    private int moneyToBeAdded = 0;
     private int allMoney;
+    private int successfullOrderTracker = 0;
 
     private void addeableMoney()
     {
@@ -20,9 +21,10 @@ public class moneyCounter : MonoBehaviour
 
     private void updateMoney()
     {
-        if (makeOrder.instance.success().isCalled)
+        if (makeOrder.instance.successfullOrders > successfullOrderTracker)
         {
-            addMoney(moneyToBeAdded)
+            addMoney(moneyToBeAdded);
+            successfullOrderTracker = makeOrder.instance.successfullOrders;
         }
     }
 
