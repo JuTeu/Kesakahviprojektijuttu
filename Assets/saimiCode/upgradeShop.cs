8cd1c56efcee708a267392d37a34484dd88c167a
SaimiVilkkilä <90615101+pupuDeveloper@users.noreply.github.com>
2023-06-16 10:40:16 +0300

    rahan vähennys päivityksen oston yhteydessä lisätty

--- Assets/saimiCode/upgradeShop.cs
+++ Assets/saimiCode/upgradeShop.cs
@@ -7,6 +7,8 @@ public class upgradeShop : MonoBehaviour
 {
     public int upgrade1Cost;
     public GameObject table;
+    public bool upgradeBought;
+    public int lastBoughtUpgrade = 0;
 
 
     public void unlockUpgrade1()
@@ -14,6 +16,23 @@ public class upgradeShop : MonoBehaviour
         if (moneyCounter.allMoney >= upgrade1Cost)
         {
             table.GetComponent<SpriteRenderer>().color = new Color32(255,255,0,100);
+            upgradeBought = true;
+            lastBoughtUpgrade = 1;
         }
+    }
+
+    public int sendUpgradeToCounter()
+    {
+        if (upgradeBought && lastBoughtUpgrade != 0)
+        {
+            switch (lastBoughtUpgrade)
+            {
+                case 1:
+                return upgrade1Cost;
+            }
+        }
+        upgradeBought = false;
+        lastBoughtUpgrade = 0;
+    return 0;
     }
 }
