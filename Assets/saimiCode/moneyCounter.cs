f4fdd69b0f84aa2096068280fc30fbbf94896917
SaimiVilkkilä <90615101+pupuDeveloper@users.noreply.github.com>
2023-06-16 09:18:52 +0300

    nopee scripti korjaus

--- Assets/saimiCode/moneyCounter.cs
+++ Assets/saimiCode/moneyCounter.cs
@@ -3,34 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using Unity.UI;
-using UnityEngine.SceneManagement;
+
 public class moneyCounter : MonoBehaviour
 {
-    [SerializeField] TextMeshProUGUI moneyText;
+    public TextMeshProUGUI moneyText;
     private int moneyToBeAdded = 0;
     private int allMoney;
     private int successfullOrderTracker = 0;
 
-    // TODO:
-    //alota FixedUpdate vasta kun tilausten teko näkymä on avattu.
+    private void Awake()
+    {
+        moneyText.text = "coins: " + allMoney;
+    }
+
+    //TODO: alota FixedUpdate vasta kun tilausten teko näkymä on avattu.
     //Tää olisi mahdollista (ehkä) gamemanagerista kun hakee levelmanagerin ja levelnamen, mutta
     //tätä ei saanut sen protection levelin takia (juho, mitä tehdä? ensun scriptei ala ite sorkkii etten riko mitää:D)
 
-    //LevelManager levelManager;
-
-    /*void Start()
-    {
-        levelManager = GetComponent<LevelManager>();
-    }*/
     private void FixedUpdate()
     {
-        //if (GameManager.Instance.levelManager.OpenLevel == "saimiTestScene")
-        //{
-            addeableMoney();
-            updateMoney();
-            addeableMoney();
-        //}
+        addeableMoney();
+        updateMoney();
+        deleteAddeableMoney();
     }
+
     private void addeableMoney()
     {
         if (makeOrder.isAnyOrderActive)
