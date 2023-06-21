cc7fbf8c098b53d2328de05ba2b318359e66bfe8
saimivilkkila <saimi.vilkkila@gmail.com>
2023-06-21 15:13:19 +0300

    infolaatikko toimii

--- Assets/saimiCode/collectiblesInteractions.cs
+++ Assets/saimiCode/collectiblesInteractions.cs
@@ -1,16 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.EventSystems;
 
 public class collectiblesInteractions : MonoBehaviour
 {
     public GameObject infoBox;
+    public TextMeshProUGUI infoText;
+    private string selectedCollectible;
     public void openInfoBox()
     {
         infoBox.SetActive(true);
+        selectedCollectible = EventSystem.current.currentSelectedGameObject.name;
+        infoTextContents();
     }
     public void closeInfoBox()
     {
         infoBox.SetActive(false);
+        selectedCollectible = "";
+    }
+
+    private void infoTextContents()
+    {
+        switch (selectedCollectible)
+        {
+            case "collectible1":
+            infoText.text = "Information of collectible1, writing sample text to test the resisable infobox :)";
+            break;
+            
+            case "collectible2":
+            infoText.text = "Information of collectible2 writing sample text to test the resisable infobox :) writing sample text to test the resisable infobox :) writing sample text to test the resisable infobox :)";
+            break;
+
+            case "collectible3":
+            infoText.text = "Information of collectible3 writing sample text to test the resisable infobox :) writing sample text to test the resisable infobox :) writing sample text to test the resisable infobox :) writing sample text to test the resisable infobox :) writing sample text to test the resisable infobox :) writing sample text to test the resisable infobox :)";
+            break;
+        }
     }
 }
