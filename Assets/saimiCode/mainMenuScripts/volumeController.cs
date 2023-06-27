c4e4895c18c2816299f242e356212f91784a2ed0
SaimiVilkkilä <90615101+pupuDeveloper@users.noreply.github.com>
2023-06-27 11:17:49 +0300

    äänislaideri tehty ja ääni tallentuu playerpreffeihin

--- Assets/saimiCode/mainMenuScripts/volumeController.cs
+++ Assets/saimiCode/mainMenuScripts/volumeController.cs
@@ -9,8 +9,27 @@ public class volumeController : MonoBehaviour
     [SerializeField] private Slider volumeSlider = null;
     [SerializeField] private TMP_Text volumeTextUI = null;
 
+    private void Start()
+    {
+        loadValues();
+    }
+
     public void volumeSliderFunc(float volume)
     {
         volumeTextUI.text = volumeSlider.value.ToString("0.00");
+    }
+
+    public void saveVolume()
+    {
+        float volumeValue = volumeSlider.value;
+        PlayerPrefs.SetFloat("VolumeValue", volumeValue);
+        loadValues();
+    }
+
+    void loadValues()
+    {
+        float volumeValue = PlayerPrefs.GetFloat("VolumeValue");
+        volumeSlider.value = volumeValue;
+        AudioListener.volume = volumeValue;
     }
 }
