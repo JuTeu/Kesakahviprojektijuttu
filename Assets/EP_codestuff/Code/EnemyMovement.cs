3912a99a7a7d4d7e3c154c36b1b5afdbda2e46e9
Esa-Pekka Palonen <esa-pekka.palonen@wpk.tpu.fi>
2023-06-30 14:08:17 +0300

    leveld

--- Assets/EP_codestuff/Code/EnemyMovement.cs
+++ Assets/EP_codestuff/Code/EnemyMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Scripting;
 
 public class EnemyMovement : MonoBehaviour
 {
@@ -11,12 +12,14 @@ public class EnemyMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // alla oleva koodirivi, mikäli halutaan nopeuden olevan random
+        //moveSpeed = Random.Range(1f, 10f);
+
     }
 
     // Update is called once per frame
     void Update()
-    {   
+    {
         // Check the patrol destination of the enemy
         if (patrolDestination == 0)
         {
