ad0090e61164ae2db6befe7ec0d862fa74714c91
Esa-Pekka Palonen <esa-pekka.palonen@wpk.tpu.fi>
2023-07-11 14:32:35 +0300

    leveldesign

---
+++ Assets/LevelChange.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Unity.VisualScripting;
+
+
+
+
+    // scripti joka vaihtaa levelin seuraavaan, kun on t�rm�tty oikeaan esineeseen
+    public class LevelChange : MonoBehaviour
+    {
+        [SerializeField] public string goNextScene;
+
+        public void OnTriggerEnter2D()
+        {
+            SceneManager.LoadScene(goNextScene);
+        }
+
+
+
+
+    }
+
