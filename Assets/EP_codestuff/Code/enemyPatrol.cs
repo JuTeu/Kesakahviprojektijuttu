ffec0f97a5d27995fafc62ecbec39c83228e344f
Esa-Pekka Palonen <esa-pekka.palonen@wpk.tpu.fi>
2023-07-11 11:17:48 +0300

    level desing

--- Assets/EP_codestuff/Code/enemyPatrol.cs
+++ Assets/EP_codestuff/Code/enemyPatrol.cs
@@ -1,33 +1,38 @@
 using UnityEngine;
+
 public class enemyPatrol : MonoBehaviour
 {
     public Transform startPoint;
     public Transform endPoint;
     public float speed = 2f;
+    public float targetThreshold = 0.1f; // Threshold for considering the enemy to have reached the target position
 
-    private float targetPosition;
+    private Vector3 targetPosition;
     private int direction = 1; // Initial direction: 1 for moving forward, -1 for moving backward
+    private bool isAtEnd = false; // Flag to track if the enemy is at the end point
 
     private void Start()
     {
         transform.position = startPoint.position;
-        targetPosition = endPoint.position.x;
+        targetPosition = endPoint.position;
     }
 
     private void Update()
     {
         float step = speed * Time.deltaTime * direction;
-        transform.position = Vector2.MoveTowards(transform.position, new Vector2(targetPosition, transform.position.y), step);
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, step);
 
-        if (transform.position.x == targetPosition)
+        if (Vector3.Distance(transform.position, targetPosition) <= targetThreshold)
         {
-            if (targetPosition == endPoint.position.x)
+            if (isAtEnd)
             {
-                targetPosition = startPoint.position.x;
+                targetPosition = startPoint.position;
+                isAtEnd = false;
             }
             else
             {
-                targetPosition = endPoint.position.x;
+                targetPosition = endPoint.position;
+                isAtEnd = true;
             }
             direction *= -1;
         }
