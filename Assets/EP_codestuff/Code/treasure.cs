6d6a9463eb333e5842c137fbb250e27489bf3b24
Esa-Pekka Palonen <esa-pekka.palonen@wpk.tpu.fi>
2023-07-11 10:50:38 +0300

    level desing

--- Assets/EP_codestuff/Code/treasure.cs
+++ Assets/EP_codestuff/Code/treasure.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class treasure : MonoBehaviour
+public class Treasure : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] GameObject gameObject;
+    bool reward1 = false;
+    bool reward2 = false;
+    bool reward3 = false;
 
-    // Update is called once per frame
-    void Update()
+    bool reward4 = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.CompareTag("Player"))
+        {
+            reward1 = true;
+        }
+        if (collision.CompareTag("Player"))
+        {
+            reward2 = true;
+        }
+        if (collision.CompareTag("Player"))
+        {
+            reward3 = true;
+        }
+        if (collision.CompareTag("Player"))
+        {
+            reward4 = true;
+        }
+        Debug.Log("palkinko1: " +reward1);
+        Debug.Log("palkinto2; " +reward2);
+        Debug.Log("palkinto3; " +reward3);
+        Debug.Log("palkinto3; " +reward4);
     }
 }
